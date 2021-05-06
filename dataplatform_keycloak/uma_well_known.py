409e77e9dc475da7f827dd2e13ab63800e611b90
Simen Heggestøyl <simen.heggestoyl@origo.oslo.kommune.no>
2021-05-06 12:14:17 +0200

    Port to Keycloak 12.0.4
    Use `introspection_endpoint` instead of the removed
    `token_introspection_endpoint`.
    
    From Keycloak's release notes [1]:
    
      In previous versions, Keycloak advertized two introspection endpoints:
      token_introspection_endpoint and introspection_endpoint. The latter is
      the one defined by RFC-8414. The former, previously deprecated, has
      now been removed.
    
    [1] https://www.keycloak.org/docs/latest/upgrading/#non-standard-token-introspection-endpoint-removed

--- dataplatform_keycloak/uma_well_known.py
+++ dataplatform_keycloak/uma_well_known.py
@@ -8,7 +8,7 @@ class UMAWellKnown:
     issuer: str
     authorization_endpoint: str
     token_endpoint: str
-    token_introspection_endpoint: str
+    introspection_endpoint: str
     resource_registration_endpoint: str
     permission_endpoint: str
     policy_endpoint: str
@@ -23,7 +23,7 @@ def get_well_known(server_url, realm) -> UMAWellKnown:
         issuer=well_known["token_endpoint"],
         authorization_endpoint=well_known["authorization_endpoint"],
         token_endpoint=well_known["token_endpoint"],
-        token_introspection_endpoint=well_known["token_introspection_endpoint"],
+        introspection_endpoint=well_known["introspection_endpoint"],
         resource_registration_endpoint=well_known["resource_registration_endpoint"],
         permission_endpoint=well_known["permission_endpoint"],
         policy_endpoint=well_known["policy_endpoint"],
