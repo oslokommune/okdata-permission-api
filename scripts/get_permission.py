4c4300f7e33a36ba7808a14ee5ea478a5c3bbe8b
Simen Heggestøyl <simen.heggestoyl@origo.oslo.kommune.no>
2022-05-19 10:42:26 +0200

    Add a couple of util scripts
    Add two utility scripts for:
    
    - Updating a Keycloak permission by raw JSON.
    
    - Printing a single Keycloak permission.

---
+++ scripts/get_permission.py
@@ -0,0 +1,32 @@
+"""Script for printing a single Keycloak permission."""
+
+import argparse
+import logging
+
+from scripts.utils import resource_server_from_env
+
+
+logger = logging.getLogger("get_permission")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--env",
+        required=True,
+        choices=["local", "dev", "prod"],
+    )
+    parser.add_argument(
+        "--permission-name", required=True, help="Name of resource to get"
+    )
+    parser.add_argument(
+        "--log-level",
+        default="INFO",
+        choices=list(logging._nameToLevel.keys()),
+    )
+    args = parser.parse_args()
+
+    logging.basicConfig(level=logging.getLevelName(args.log_level))
+
+    resource_server = resource_server_from_env(args.env)
+    print(resource_server.get_permission(args.permission_name))
