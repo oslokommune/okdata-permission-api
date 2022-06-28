ab8bfc7a6ba0fd51f5dfd74dda7c6f5e74cfcc39
Simen Heggestøyl <simen.heggestoyl@origo.oslo.kommune.no>
2022-06-28 12:28:08 +0200

    Merge `has_permission` and `has_scope_permission`
    They were doing basically the same thing, but `has_permission` had
    better error handling, while `has_scope_permission` had the better
    name.

--- resources/resources.py
+++ resources/resources.py
@@ -6,7 +6,7 @@ from requests.exceptions import HTTPError
 
 from dataplatform_keycloak.resource_server import ResourceServer
 from models import CreateResourceBody
-from resources.authorizer import has_permission
+from resources.authorizer import has_scope_permission
 from resources.errors import ErrorResponse, error_message_models
 
 logger = logging.getLogger()
@@ -22,7 +22,7 @@ router = APIRouter()
 
 @router.post(
     "",
-    dependencies=[Depends(has_permission("keycloak:resource:admin"))],
+    dependencies=[Depends(has_scope_permission("keycloak:resource:admin"))],
     status_code=status.HTTP_201_CREATED,
     responses=error_message_models(
         status.HTTP_400_BAD_REQUEST,
@@ -54,7 +54,7 @@ def create_resource(
 
 @router.delete(
     "/{resource_name}",
-    dependencies=[Depends(has_permission("keycloak:resource:admin"))],
+    dependencies=[Depends(has_scope_permission("keycloak:resource:admin"))],
     status_code=status.HTTP_200_OK,
     responses=error_message_models(
         status.HTTP_400_BAD_REQUEST,
