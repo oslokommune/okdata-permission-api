38d6998acc3dd6837e8a8f755b0765d32a16defb
Simen Heggestøyl <simen.heggestoyl@dig.oslo.kommune.no>
2026-03-31 14:10:42 +0200

    Bump `boto3`, `moto`, and `urllib3`
    Bump `moto` to be able to bump `boto3` to be able to bump `urllib3` to
    remove vulnerabilities.

--- tests/jobs/test_backup.py
+++ tests/jobs/test_backup.py
@@ -5,6 +5,7 @@ from datetime import datetime
 
 import boto3
 from freezegun import freeze_time
+from moto import mock_aws
 
 from dataplatform_keycloak.resource_server import ResourceServer
 from jobs.backup import (
@@ -12,9 +13,34 @@ from jobs.backup import (
     BACKUP_BUCKET_PREFIX,
     backup_permissions,
     load_latest_backup,
+    write_to_s3,
 )
 from models import User
 from tests.setup import populate_local_keycloak
+
+
+def _mock_s3():
+    s3 = boto3.resource("s3", region_name=os.environ["AWS_REGION"])
+    s3.create_bucket(
+        Bucket=os.environ["BACKUP_BUCKET_NAME"],
+        CreateBucketConfiguration={"LocationConstraint": os.environ["AWS_REGION"]},
+    )
+
+
+def _write_permission_backups():
+    permission_backups = [
+        ("2022-05-24T10:00:30", [{"foo": "bar"}, {"abc": "def"}]),
+        ("2022-05-12T11:00:30", [{"foo": "bar"}]),
+        ("2022-03-01T11:00:30", [{"foo": "bar"}]),
+        ("2021-07-01T12:00:30", [{"foo": "bar"}]),
+        ("2022-01-01T13:00:30", [{"foo": "bar"}]),
+    ]
+
+    for date, permissions in permission_backups:
+        with freeze_time(date):
+            write_to_s3(permissions)
+
+    return permission_backups
 
 
 def _prefix_from_datetime(backup_datetime):
@@ -26,7 +52,9 @@ def _prefix_from_datetime(backup_datetime):
     )
 
 
-def test_backup_permissions(mock_ssm_client, mock_s3_bucket):
+@mock_aws
+def test_backup_permissions(mock_ssm_client):
+    _mock_s3()
     populate_local_keycloak.populate()
     rs = ResourceServer()
     janedoe_user = User.parse_obj({"user_id": "janedoe", "user_type": "user"})
@@ -44,7 +72,11 @@ def test_backup_permissions(mock_ssm_client, mock_s3_bucket):
     case.assertCountEqual(backed_up_permissions, rs.list_permissions())
 
 
-def test_s3_prefixes(mock_s3_bucket, permission_backups):
+@mock_aws
+def test_s3_prefixes():
+    _mock_s3()
+    permission_backups = _write_permission_backups()
+
     s3 = boto3.client("s3", region_name=os.environ["AWS_REGION"])
     object_keys = [
         obj["Key"] for obj in s3.list_objects_v2(Bucket=BACKUP_BUCKET_NAME)["Contents"]
@@ -58,13 +90,21 @@ def test_s3_prefixes(mock_s3_bucket, permission_backups):
     )
 
 
-def test_load_latest_backup(mock_s3_bucket, permission_backups):
+@mock_aws
+def test_load_latest_backup():
+    _mock_s3()
+    _write_permission_backups()
+
     with freeze_time("2022-05-25T12:00:00+00:00"):
         permissions = load_latest_backup()
     assert len(permissions) == 2
 
 
-def test_load_latest_backup_none_within_max_age(mock_s3_bucket, permission_backups):
+@mock_aws
+def test_load_latest_backup_none_within_max_age():
+    _mock_s3()
+    _write_permission_backups()
+
     with freeze_time("2022-8-26T17:00:00+00:00"):
         permissions = load_latest_backup(max_age_in_weeks=4)
     assert permissions is None
